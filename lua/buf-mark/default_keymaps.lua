99d8546b17c0f698f461ece31bf5c84777623811
Ryan Burda <ryanburda@gmail.com>
2026-09-06 23:45:09 -0400

    default keymaps

--- lua/buf-mark/default_keymaps.lua
+++ lua/buf-mark/default_keymaps.lua
@@ -2,8 +2,9 @@ local T = {}
 
 T.setup = function()
   local buf_mark = require('buf-mark')
-  vim.keymap.set('n', '<leader>m', buf_mark.set, { desc = 'BufMark: Set' })
   vim.keymap.set('n', "<leader>'", buf_mark.goto, { desc = 'BufMark: Goto' })
+  vim.keymap.set('n', '<leader>m', buf_mark.set, { desc = 'BufMark: Set' })
+  vim.keymap.set('n', "<leader>M", buf_mark.delete, { desc = 'BufMark: Delete' })
 end
 
 return T
